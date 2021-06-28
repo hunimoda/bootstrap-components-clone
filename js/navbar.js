b398bafe3b9ca445ce1e86fa079f645e615a10a7
hunimoda <hunimoda@gmail.com>
2021-06-29 04:07:32 +0900

    5. Added Simple Top-Navbar with CSS Transition & JS

---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+function addEventListenerToAll(event, eventHandler, targets) {
+    for (let i = 0; i < targets.length; i++)
+        targets[i].addEventListener(event, eventHandler);
+}
+
+function onTopNavbarAnchorClick(event) {
+    event.preventDefault();
+
+    const currentLi = event.currentTarget.parentElement;
+    if (currentLi.classList.contains(ACTIVE))
+        return;
+
+    const allLis = currentLi.parentElement.getElementsByTagName('li');
+    // remove 'active' from all li(s)
+    for (let i = 0; i < allLis.length; i++)
+        allLis[i].classList.remove(ACTIVE);
+
+    currentLi.classList.add(ACTIVE);
+}
+
+function mainApp() {
+    const topNavbarAnchors = document.querySelectorAll('.top-navbar a');
+    addEventListenerToAll('click', onTopNavbarAnchorClick, topNavbarAnchors);
+}
+mainApp();
