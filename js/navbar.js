42d89da515052f832fda32d63364d17dd101aa6d
hunimoda <hunimoda@gmail.com>
2021-06-29 17:04:58 +0900

    6. Learn Us Clone - Side Navbar with JS

--- js/navbar.js
+++ js/navbar.js
@@ -18,8 +18,23 @@ function onTopNavbarAnchorClick(event) {
     currentLi.classList.add(ACTIVE);
 }
 
+function sideNavbarToggle(event) {
+    const sideNavbar = document.querySelector('.side-navbar__container .side-navbar');
+    const content = document.querySelector('.side-navbar__container .page-content');
+    const screenMask = document.querySelector('.side-navbar__container .screen-mask');
+
+    sideNavbar.classList.toggle(ACTIVE);
+    content.classList.toggle(ACTIVE);
+    screenMask.classList.toggle(ACTIVE);
+}
+
 function mainApp() {
     const topNavbarAnchors = document.querySelectorAll('.top-navbar a');
     addEventListenerToAll('click', onTopNavbarAnchorClick, topNavbarAnchors);
+
+    const sideNavbarOpener = document.querySelector('.side-navbar__container .fa-bars');
+    sideNavbarOpener.addEventListener('click', sideNavbarToggle);
+    const sideNavbarScreenMask = document.querySelector('.side-navbar__container .screen-mask');
+    sideNavbarScreenMask.addEventListener('click', sideNavbarToggle);
 }
 mainApp();
